3241ffe7eff18795e15e69d130da179e2fd8d009
maochunsun <maochuns.sun@gmail.com>
2020-05-05 18:44:23 +0800

    Update ViewController.swift

--- Example/secux-paymentkit/ViewController.swift
+++ Example/secux-paymentkit/ViewController.swift
@@ -178,7 +178,10 @@ class ViewController: UIViewController {
         //Pay to store
         //let paymentInfo = "{\"amount\":\"1.5\", \"coinType\":\"DCT\", \"token\":\"SPC\",\"deviceIDhash\":\"f962639145992d7a710d33dcca503575eb85d759\"}"
         
-        let paymentInfo = "{\"amount\":\"2\", \"coinType\":\"DCT:SPC\",\"deviceIDhash\":\"592e41d67ee326f82fd6be518fd488d752f5a1b9\"}"
+        //let paymentInfo = "{\"amount\":\"2\", \"coinType\":\"DCT:SPC\",\"deviceIDhash\":\"592e41d67ee326f82fd6be518fd488d752f5a1b9\"}"
+        
+        let paymentInfo = "{\"amount\":\"2\", \"coinType\":\"DCT:SPC\",\"deviceIDhash\":\"f962639145992d7a710d33dcca503575eb85d759\"}"
+        
         let (ret, data) = paymentManager.getPaymentInfo(paymentInfo: paymentInfo)
         if ret == SecuXRequestResult.SecuXRequestOK, let data = data{
             print("get payment info. done")

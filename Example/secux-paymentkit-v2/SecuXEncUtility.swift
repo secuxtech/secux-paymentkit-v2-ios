19325f52b8f82ec9e11b65edb9db7a733ffc2692
MaochunS <52019408+MaochunS@users.noreply.github.com>
2020-12-25 18:16:10 +0800

    Update SecuXEncUtility.swift

--- Example/secux-paymentkit-v2/SecuXEncUtility.swift
+++ Example/secux-paymentkit-v2/SecuXEncUtility.swift
@@ -155,20 +155,37 @@ open class SecuXEncUtility {
     
     
     func MD5ToData(string: String) -> Data {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        let messageData = string.data(using:.utf8)!
-        var digestData = Data(count: length)
+        
+        
+        if #available(iOS 13.0, *){
+            
+            //let digest = Insecure.MD5.hash(data: string.data(using: .utf8) ?? Data())
+            //return Data(digest)
+            
+            return Data()
+            
+        }else{
+    
+            let length = Int(CC_MD5_DIGEST_LENGTH)
+            let messageData = string.data(using:.utf8)!
+            var digestData = Data(count: length)
 
-        _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
-            messageData.withUnsafeBytes { messageBytes -> UInt8 in
-                if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
-                    let messageLength = CC_LONG(messageData.count)
-                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+            _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+                messageData.withUnsafeBytes { messageBytes -> UInt8 in
+                    if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                        let messageLength = CC_LONG(messageData.count)
+                        
+                        
+                        CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                        
+                    }
+                    return 0
                 }
-                return 0
             }
-        }
-        return digestData
+            
+            return digestData
+       // }
+        
     }
     
     
